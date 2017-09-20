b88e6b345e0f7d514ce9c5fb059f2b5477e8c897
varo2397 <alvarocastro74@gmail.com>
2017-09-20 11:42:56 -0600

    Archivos basicos

---
+++ Progra1/Progra1/Controllers/VendedorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Progra1.Controllers
+{
+    public class VendedorController : Controller
+    {
+        public ActionResult InicioSesion()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Servicios()
+        {
+            return View();
+        }
+
+        public ActionResult Registrarse()
+        {
+            return PartialView();
+        }
+
+        public ActionResult AgregarServicio()
+        {
+            return View();
+        }
+    }
+}
