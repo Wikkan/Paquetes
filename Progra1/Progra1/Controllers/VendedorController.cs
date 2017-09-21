81f851c18cb88d75026ecff328d5edd06a7ebde6
varo2397 <alvarocastro74@gmail.com>
2017-09-21 02:27:46 -0600

    merge

--- Progra1/Progra1/Controllers/VendedorController.cs
+++ Progra1/Progra1/Controllers/VendedorController.cs
@@ -27,5 +27,17 @@ namespace Progra1.Controllers
         {
             return View();
         }
+        
+        public string cantidadTarifas(int cantidad)
+        {
+            string htmltarifas = "";
+            for(int i = 0; i <= cantidad; i++)
+            {
+                string tarifa = "<div class=\"form - group\"> < h1 > Tarifa " + (i + 1) + "</ h1 ></ div >< div class=\"form-group\"><label>Nombre tarifa " + (i + 1) + "</label></div><div class=\"form-group\"><input type = \"text\" class=\"form-control\" required></div><div class=\"form-group\"><label>Descripcion tarifa " + (i + 1) + "</label></div><div class=\"form-group\"><textarea class=\"form-control\" required></textarea></div><div class=\"form-group\"><label>Precio de la tarifa" + (i + 1) + "</label></div><div class=\"form-group\"><input type = \"number\" class=\"form-control\" min=\"1\" max =\"10\" required ></div>";
+                htmltarifas += tarifa;
+            }
+
+            return htmltarifas;
+        }
     }
 }
